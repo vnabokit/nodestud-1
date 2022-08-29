98951be2b6d88f02062a8d4a3502f9965f2b6f5e
Vova <vnabokit@gmail.com>
2022-08-29 15:36:47 +0300

    291

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
+console.log("process.env:", process.env);
+console.log("process.env.NODE_ENV:", process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
 } else if (process.env.NODE_ENV === 'ci') {
